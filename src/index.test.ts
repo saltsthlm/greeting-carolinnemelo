3f601b4cb7bf2433cbb5f78bd51a299a31aa8d05
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-17 17:05:48 +0200

    make function that returns object with message as key and expect value from test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
 import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
-function greet() {}
+function greet(name: string) {
+  const greet = {
+    message: `Welcome, ${name}!`
+
+  }
+  return greet
+}
 
 test("should greet person", () => {
   const result = greet("Alek");
